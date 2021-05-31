8ea6cd3af0adc41cf10ce6fab3c132c094193ade
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-05-31 19:18:00 +0300

    Initial commit

---
+++ CinemaWrld.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CinemaWrld.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
