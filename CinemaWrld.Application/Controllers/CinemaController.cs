015130e3a912a7c79b28e6927a3ef1882a480315
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-05-31 21:07:15 +0300

    Add controllers and views

---
+++ CinemaWrld.Application/Controllers/CinemaController.cs
@@ -0,0 +1,41 @@
+﻿using CinemaWrld.Application.Data;
+using CinemaWrld.Application.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWrld.Application.Controllers
+{
+    public class CinemaController : Controller
+    {
+
+        private ApplicationDbContext dbcontex;
+
+        public CinemaController(ApplicationDbContext dbContext)
+        {
+            this.dbcontex = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            Cinema cinema = new Cinema();
+
+            cinema.Name = "Palace";
+            cinema.Location = "Sofia";
+            cinema.PhoneNumber = "54646545464";
+            cinema.Picture = "a";
+
+            dbcontex.Cinemas.Add(cinema);
+            dbcontex.SaveChanges();
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ CinemaWrld.Application/Models/CinemaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWrld.Application.Models
+{
+    public class CinemaViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public string Location { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public IFormFile PictureImg { get; set; }
+    }
+}
