988d64bc63ba942f42a7b3d80666c35d9b30069d
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-06-02 19:59:06 +0300

    Create MakingController.cs

---
+++ CinemaWrld.Application/Areas/Making/Controllers/MakingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWrld.Application.Areas.Making.Controllers
+{
+    [Area("Making")]
+    public abstract class MakingController : Controller
+    {
+    }
+}
