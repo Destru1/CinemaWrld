c288af11146744f0640e08cb964e1ae2f53a042f
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-06-07 11:56:40 +0300

    Create GetAllActorsViewModel.cs

---
+++ CinemaWrld.Application/Areas/Making/Models/Actors/ViewModels/GetAllActorsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWrld.Application.Areas.Making.Models.Actors.ViewModels
+{
+    public class GetAllActorsViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string MovieName { get; set; }
+
+        public string PlayingAs { get; set; }
+    }
+}
