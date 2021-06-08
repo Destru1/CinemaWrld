2494dae932f61fd84c8d92e7e33466593ff86682
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-06-08 16:16:17 +0300

    Create NotificationsConstants.cs

---
+++ CinemaWrld.Application/Constants/NotificationsConstants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaWrld.Application.Constants
+{
+    public class NotificationsConstants
+    {
+        public const string SUCCESS_NOTIFICATION = "Success";
+        public const string ERROR_NOTIFICATION = "Error";
+        public const string WARNING_NOTIFICATION = "Warning";
+        public const string INFORMATION_NOTIFICATION = "Info";
+
+
+        public const string SUCCESSFULLY_ADDED_MOVIE = "Successfully added movie!";
+        public const string SUCCESSFULLY_ADDED_ACTOR = "Successfully added actor!";
+        public const string SUCCESSFULLY_ADDED_DIRECTOR = "Successfully added director!";
+
+
+
+
+    }
+}
