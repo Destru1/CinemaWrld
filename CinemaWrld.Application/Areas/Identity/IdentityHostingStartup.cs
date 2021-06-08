cfaad2833d8150a256d0eb5f85d41bc866374b0f
Destru1 <68596367+Destru1@users.noreply.github.com>
2021-06-08 16:16:08 +0300

    Scaffold login and register

---
+++ CinemaWrld.Application/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CinemaWrld.Application.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CinemaWrld.Application.Areas.Identity.IdentityHostingStartup))]
+namespace CinemaWrld.Application.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
